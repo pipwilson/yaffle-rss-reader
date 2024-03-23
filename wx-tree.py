5ed7b592f867a1ee57fe0b19056d48d745a7f894
Phil Wilson <github@philwilson.org>
2024-03-23 18:50:04 +0000

    More playing with tree behaviour

--- wx-tree.py
+++ wx-tree.py
@@ -13,40 +13,41 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, parent, -1, "TreeCtrl Demo")
 
         # Create a TreeCtrl with expand/collapse buttons
-        self.tree = wx.TreeCtrl(self, style=wx.TR_HIDE_ROOT | wx.TR_HAS_BUTTONS | wx.TR_FULL_ROW_HIGHLIGHT)
-        root = self.tree.AddRoot('Feeds')
+        self.tree = wx.TreeCtrl(self, style=wx.TR_HIDE_ROOT | wx.TR_NO_LINES | wx.TR_HAS_BUTTONS | wx.TR_FULL_ROW_HIGHLIGHT)
+
+        isz = (48,48)
+        il = wx.ImageList(isz[0], isz[1])
+        fldridx     = il.Add(wx.ArtProvider.GetBitmap(wx.ART_FOLDER,      wx.ART_OTHER, isz))
+        fileidx     = il.Add(wx.ArtProvider.GetBitmap(wx.ART_NORMAL_FILE, wx.ART_OTHER, isz))
+        self.tree.SetImageList(il)
+        self.il = il
+
+        self.root = self.tree.AddRoot('Feeds')
+        self.tree.SetItemImage(self.root, fldridx, wx.TreeItemIcon_Normal)
 
         # Add some folders (sub-items)
-        folder1 = self.tree.AppendItem(root, 'Folder 1')
-        folder2 = self.tree.AppendItem(root, 'Folder 2')
+        folder1 = self.tree.AppendItem(self.root, 'Feeds')
+        self.tree.SetItemImage(folder1, fldridx, wx.TreeItemIcon_Normal)
+
+        folder2 = self.tree.AppendItem(self.root, 'Podcasts')
+        self.tree.SetItemImage(folder2, fldridx, wx.TreeItemIcon_Normal)
+
+        folder3 = self.tree.AppendItem(folder1, 'More feeds')
+        self.tree.SetItemImage(folder3, fldridx, wx.TreeItemIcon_Normal)
 
         # Add some feeds (sub-sub-items)
-        self.tree.AppendItem(folder1, 'Feed 1.1')
-        self.tree.AppendItem(folder1, 'Feed 1.2')
-        self.tree.AppendItem(folder2, 'Feed 2.1')
-
-        imageList = wx.ImageList(16, 16)
-        # self.closed_folder = imageList.Add(wx.ArtProvider.GetBitmap(wx.ART_FOLDER, wx.ART_OTHER, (16,16)))
-        #shell32 = win32api.GetModuleFileName(win32api.GetModuleHandle('shell32.dll'))
-        #self.closed_folder = imageList.Add(wx.Icon(shell32 + ';43', wx.BITMAP_TYPE_ICO, desiredWidth=16, desiredHeight=16))
-       
-        chevron_right_path = os.path.join('.', 'chevron-right.png')
-        self.closed_folder = imageList.Add(wx.Bitmap(wx.Image(chevron_right_path, wx.BITMAP_TYPE_PNG)))
-
-        chevron_down_path = os.path.join('.', 'chevron-down.png')
-        self.open_folder = imageList.Add(wx.Bitmap(wx.Image(chevron_down_path, wx.BITMAP_TYPE_PNG)))
-
-        self.normal_file = imageList.Add(wx.ArtProvider.GetBitmap(wx.ART_NORMAL_FILE, wx.ART_OTHER, (16,16)))
-        
-        self.tree.AssignImageList(imageList)
-
-        self.tree.SetItemImage(root, self.closed_folder)
-        self.tree.SetItemImage(folder1, self.closed_folder)
-        self.tree.SetItemImage(folder2, self.closed_folder)
-        self.tree.SetItemImage(self.tree.AppendItem(folder1, 'Feed 1.1'), self.normal_file)
+        file1 = self.tree.AppendItem(folder1, 'Feed 1.1')
+        self.tree.SetItemImage(file1, fileidx, wx.TreeItemIcon_Normal)
+        file2 = self.tree.AppendItem(folder1, 'Feed 1.2')
+        self.tree.SetItemImage(file2, fileidx, wx.TreeItemIcon_Normal)
+        file3 = self.tree.AppendItem(folder1, 'Feed 2.1')
+        self.tree.SetItemImage(file3, fileidx, wx.TreeItemIcon_Normal)
+        file4 = self.tree.AppendItem(folder3, 'Feed 1.1.1')
+        self.tree.SetItemImage(file4, fileidx, wx.TreeItemIcon_Normal)
 
         self.tree.Expand(folder1)
         self.tree.Expand(folder2)
+        self.tree.Expand(folder3)
 
         self.tree.Bind(wx.EVT_LEFT_DOWN, self.on_item_activated)
 
@@ -72,9 +73,9 @@ class MyFrame(wx.Frame):
             event.Skip()
             return
 
-        # if you click the icon of a folder, expand/collapse it
+        # if you click the icon of a folder or +/-, expand/collapse it
         # otherwise select the item if it wasn't already selected
-        if((hit_test_flags & wx.TREE_HITTEST_ONITEMICON) == wx.TREE_HITTEST_ONITEMICON):
+        if(self.clicked_folder_or_expander(hit_test_flags)):
             if(self.tree.ItemHasChildren(tree_item_id)):
                 self.tree.Toggle(tree_item_id)
         else:
@@ -82,13 +83,18 @@ class MyFrame(wx.Frame):
                 self.tree.SelectItem(tree_item_id)
 
 
+    def clicked_folder_or_expander(self, hit_test_flags):
+        # if the mouse is over the expander or the icon of a folder, return True
+        # if the mouse is over the text of a folder, return False
+        return (hit_test_flags & wx.TREE_HITTEST_ONITEMICON) == wx.TREE_HITTEST_ONITEMICON or (hit_test_flags & wx.TREE_HITTEST_ONITEMBUTTON) == wx.TREE_HITTEST_ONITEMBUTTON
+
     def on_item_expanded(self, event):
         item = event.GetItem()
-        self.tree.SetItemImage(item, self.open_folder)
+        # self.tree.SetItemImage(item, self.open_folder)
 
     def on_item_collapsed(self, event):
         item = event.GetItem()
-        self.tree.SetItemImage(item, self.closed_folder)
+        # self.tree.SetItemImage(item, self.closed_folder)
 
     def on_feed_selected(self, event):
         item = event.GetItem()
